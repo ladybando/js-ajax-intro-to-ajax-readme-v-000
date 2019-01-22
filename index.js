05f2c20bc788a9f1bde3226979bb06abc772507e
Ayana Bando <ayanaflax@yahoo.com>
2019-01-22 21:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ req.addEventListener('load', showRepositories);
   req.send();
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
